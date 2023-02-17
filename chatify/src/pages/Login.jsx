3050145dc44f1d74f158b93d7a42157813325451
PRINCE-PRASAD <princeprasad.code@gmail.com>
2023-02-17 18:17:54 +0530

    Base created , Login and Signup Page

---
+++ chatify/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const Login = () => {
+    return (
+<div className="formContainer">
+    <div className="formWrapper">
+<span className="logo">Chatify</span>
+<span className="title">Login</span>
+    
+<form action="">
+    <input type="email" placeholder="email" />
+    <input type="password" placeholder="password" />
+
+    <button>Sign in</button>
+</form>
+<p>you don't have account? Register</p>
+</div>
+</div>
+    )
+}
+export default Login;
