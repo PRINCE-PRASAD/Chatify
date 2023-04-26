82c1d05dd2fd9f1b997fed585df9e247e9ca656a
PRINCE-PRASAD <princeprasad.code@gmail.com>
2023-04-26 10:44:00 +0530

    Login and Signup setup done with  bcrypt and jwt and also cheked with Thunder client

--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,36 @@ const express = require("express");
 const dotenv = require("dotenv");
 const {chats} = require("./data/data");
 const connectDB = require("./config/db")
+const userRoutes = require('./routes/userRoutes')
+const {notFound, errorHandler}= require("./middleware/errorMiddleware")
+
+
 
 dotenv.config();
 connectDB();
 const app = express();
 
+app.use(express.json());  // to acept the json data
+
 app.get("/", (req, res)=> {
     res.send("API is Running With health");
 });
 
-app.get('/api/chat', (req, res)=> {
-    res.send(chats);
-});
+app.use('/api/user',userRoutes)
 
-app.get("/api/chat/:id", (req, res)=>{
-    // console.log(req);
-    const singlechat = chats.find((c) => c._id === req.params.id);
-    res.send(singlechat)
-});
+// app.get('/api/chat', (req, res)=> {
+//     res.send(chats);
+// });
+// app.get("/api/chat/:id", (req, res)=>{
+//     // console.log(req);
+//     const singlechat = chats.find((c) => c._id === req.params.id);
+//     res.send(singlechat)
+// });
+
+
+// this two use for extra errors or if someone click different routes
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
